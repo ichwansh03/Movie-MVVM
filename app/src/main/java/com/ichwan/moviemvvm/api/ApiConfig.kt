d57dbae93980ad71e8adf5759d033c47777e6c00
ichwansh03 <ichwansholihin03@gmail.com>
2023-08-25 19:02:20 +0700

    chore (api): add ApiConfig.kt

---
+++ app/src/main/java/com/ichwan/moviemvvm/api/ApiConfig.kt
@@ -0,0 +1,32 @@
+package com.ichwan.moviemvvm.api
+
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import com.ichwan.moviemvvm.BuildConfig
+
+/**
+ * nge-provide konfigurasi antar API
+ */
+class ApiConfig {
+
+    companion object {
+        fun getApiService(): ApiService {
+            val logInceptor = if (BuildConfig.DEBUG) HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY)
+            else HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.NONE)
+
+            val client = OkHttpClient.Builder()
+                .addInterceptor(logInceptor)
+                .build()
+
+            val retrofit = Retrofit.Builder()
+                .baseUrl(BuildConfig.BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .client(client)
+                .build()
+
+            return retrofit.create(ApiService::class.java)
+        }
+    }
+}
