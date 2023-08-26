57ea4bd6c62611ff99e97d807d60718399029ce4
ichwansh03 <ichwansholihin03@gmail.com>
2023-08-26 20:27:42 +0700

    chore (api): add endpoint search movies and genres

--- app/src/main/java/com/ichwan/moviemvvm/api/ApiService.kt
+++ app/src/main/java/com/ichwan/moviemvvm/api/ApiService.kt
@@ -1,11 +1,13 @@
 package com.ichwan.moviemvvm.api
 
+import com.ichwan.moviemvvm.models.GenreResponse
 import com.ichwan.moviemvvm.models.MovieResponse
+import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Query
 
 /**
- * nge-provide endpoint dari API
+ * menyediakan konfigurasi endpoint dari API
  */
 
 interface ApiService {
@@ -14,5 +16,17 @@ interface ApiService {
     suspend fun getPopularMovie(
         @Query("api_key") key: String?,
         @Query("page") page: Int?
-    ): MovieResponse
+    ): Response<MovieResponse>
+
+    @GET("search/movie")
+    suspend fun getSearchMovie(
+        @Query("api_key") key: String?,
+        @Query("query") query: String?,
+        @Query("page") page: Int?
+    ): Response<MovieResponse>
+
+    @GET("genre/movie/list")
+    suspend fun getGenres(
+        @Query("api_key") key: String?
+    ): GenreResponse
 }
